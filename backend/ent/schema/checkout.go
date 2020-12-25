4c93fb2e9adb328ecbcdbc5c66022568d74d0986
ISSARAPAP7 <film4777@gmail.com>
2020-12-26 01:39:37 +0700

    สร้าง field ของ checkout - close #3

---
+++ backend/ent/schema/checkout.go
@@ -0,0 +1,23 @@
+package schema
+
+import (
+
+    "github.com/facebook/ent"
+    "github.com/facebook/ent/schema/field"
+)
+// Checkout holds the schema definition for the Checkout entity.
+type Checkout struct {
+	ent.Schema
+}
+
+// Fields of the Checkout.
+func (Checkout) Fields() []ent.Field {
+	return []ent.Field{
+        field.Time("Create_at"),
+    }
+}
+
+// Edges of the Checkout.
+func (Checkout) Edges() []ent.Edge {
+	return nil
+}
---
+++ backend/go.mod
@@ -0,0 +1,8 @@
+module github.com/B6131685/app
+
+go 1.15
+
+require (
+	github.com/facebook/ent v0.5.3
+	github.com/facebookincubator/ent v0.2.7
+)
